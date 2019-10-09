2aaabe4913cd081c9932d750f0ad738064c16117
Qiuqiu <842792595@qq.com>
2019-10-09 12:57:09 +0800

    完成遍历省市县三级列表的功能

--- CoolWeather/app/src/main/java/com/example/administrator/coolweather/db/County.java
+++ CoolWeather/app/src/main/java/com/example/administrator/coolweather/db/County.java
@@ -8,7 +8,9 @@ import org.litepal.crud.DataSupport;
 
 public class County extends DataSupport {
     private int id;
-
+    private String countyName;
+    private String weatherId;
+    private int cityId;
     public int getCityId() {
         return cityId;
     }
@@ -41,7 +43,5 @@ public class County extends DataSupport {
         this.weatherId = weatherId;
     }
 
-    private String countyName;
-    private String weatherId;
-    private int cityId;
+
 }
